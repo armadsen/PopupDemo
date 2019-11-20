a25b88ae0360be0178c0b9e7a53946ef31b797c8
Andrew Madsen <andrew@openreelsoftware.com>
2019-11-20 09:29:55 -0700

    Initial Commit

---
+++ PopupDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PopupDemo
+//
+//  Created by Andrew R Madsen on 11/20/19.
+//  Copyright © 2019 Open Reel Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
